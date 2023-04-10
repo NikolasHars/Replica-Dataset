231a136807a87544c4a7098562889cbdd4337f44
Nikolas Hars <nihars@eu-login-01.euler.ethz.ch>
2023-04-10 17:46:35 +0200

    Pan through image on quasi-random rotation. Added intrinsics savin toit

--- ReplicaSDK/src/render.cpp
+++ ReplicaSDK/src/render.cpp
@@ -12,6 +12,11 @@
 #include <map>
 #include <vector>
 #include <stdio.h>
+#include <random>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <random>
 
 #include "GLCheck.h"
 #include "MirrorRenderer.h"
@@ -19,7 +24,7 @@
 using namespace std;
 using namespace Eigen;
 
-#define PI 3.14159265
+#define DEG2RAD 3.14159265 / 180.0
 
 std::string YAML_FILE = "/cluster/project/infk/courses/252-0579-00L/group04/processed_data/conf.yaml";
 auto CONFIG = YAML::LoadFile(YAML_FILE);
@@ -38,6 +43,74 @@ void saveData(string fileName, MatrixXd  matrix, string img_name)
     }
 }
 
+// Generates two Eigen vectors that are perpendicular to a specified vector and to each other
+void generatePerpendicularVectors(const Eigen::Vector3d& vector, Eigen::Vector3d& v1, Eigen::Vector3d& v2)
+{
+    // Generate a random vector
+    Eigen::Vector3d random_vector = Eigen::Vector3d::Random();
+
+    // Project the random vector onto the plane perpendicular to the specified vector
+    Eigen::Vector3d projected_vector = random_vector - random_vector.dot(vector) * vector;
+
+    // Set the first perpendicular vector to the normalized projected vector
+    v1 = projected_vector.normalized();
+
+    // Set the second perpendicular vector to the cross product of the specified vector and the first perpendicular vector
+    v2 = vector.cross(v1).normalized();
+}
+
+// Generates a random rotation matrix around two perpendicular axes
+void randomRotation(
+  const float rotation_angle, 
+  const Eigen::Vector3d& axis1, 
+  const Eigen::Vector3d& axis2,
+  const Eigen::Vector3d& translation_vector,
+  Eigen::Matrix4d& inv_rotation_matrix4d,
+  Eigen::Matrix4d& transformation_matrix)
+{
+    // Generate a random angle between 0 and random_angle
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-rotation_angle, rotation_angle);
+    
+    // generate random angles for the two axes
+    const double angle_1 = dis(gen)/5;
+    const double angle_2 = dis(gen);
+
+    // Create a rotation matrix from the first axis and angle
+    //Eigen::AngleAxisd rotation1(angle_1, axis1);
+    Eigen::AngleAxisd rotation1(angle_1, translation_vector.normalized().cross(Eigen::Vector3d::UnitZ()));
+
+    // Create a rotation matrix around the second axis
+    // Eigen::AngleAxisd rotation2(angle_2, axis2);
+    Eigen::AngleAxisd rotation2(angle_2, Eigen::Vector3d::UnitZ());
+
+    // Combine the rotation matrices
+    Eigen::Matrix3d rotation_matrix = (rotation2 * rotation1).toRotationMatrix();
+
+    // Invert the rotation matrix to be able to rotate the camera back to the original path -> no strong deviation from path
+    Eigen::Matrix3d inv_rotation_matrix3d = rotation_matrix.inverse();
+
+    // Create a 3D affine transformation from the rotation matrix and translation vector
+    Eigen::Transform<double, 3, Eigen::Affine> affine_transform;
+    affine_transform = rotation_matrix;
+    affine_transform.translation() = translation_vector;
+
+    // Convert the affine transformation to a 4x4 homogeneous transformation matrix
+    transformation_matrix = affine_transform.matrix();
+
+    // Rotate the camera back to the original path while maintaining the new incremental transformation
+    transformation_matrix *= inv_rotation_matrix4d;
+
+    // Create a 3D affine transformation from the inverse rotation matrix and 0 translation vector
+    Eigen::Transform<double, 3, Eigen::Affine> inv_affine_transform;
+    inv_affine_transform = inv_rotation_matrix3d;
+    inv_affine_transform.translation() = Eigen::Vector3d::Zero();
+
+    inv_rotation_matrix4d = inv_affine_transform.matrix();
+
+    return;
+}
 
 void saveFloat(string fileName, float exposure)
 {
@@ -50,18 +123,45 @@ void saveFloat(string fileName, float exposure)
     }
 }
 
-
-int argmax(float first, float second) {
-  if (first > second) {
-    return 0;
-  } 
-  else {
-    return 1;
-  }
+void saveYAML(string fileName, double value, string value_desc)
+{
+    ofstream file;
+    file.open(fileName, std::ios_base::app);
+    if (file.is_open())
+    {
+        file << value_desc << ": " << value << "\n";
+        file.close();
+    }
 }
 
-int main(int argc, char* argv[]) {
+/**
+ * @brief argmax of 2 float numbers
+ * @param first first float number 
+ * @param second second float number
+ * @return index of greater number
+ */
+int argmax(float first, float second) {
+  if (first > second) return 0;
+  return 1;
+}
 
+
+// Extracts the intrinsic camera parameters and stores them in a YAML file
+void storeCamParams(int w, int h, double fu, double fv, double u0, double v0, double zNear, double zFar) {
+    Eigen::Matrix4d M;
+    M << fu, 0.0, u0, 0.0,
+         0.0, fv, v0, 0.0,
+         0.0, 0.0, 1.0, 0.0,
+         0.0, 0.0, 0.0, 1.0;
+    saveData("cam_params.csv", M, "cam_params");
+    saveYAML("cam_params.yaml", w, "width");
+    saveYAML("cam_params.yaml", h, "height");
+    saveYAML("cam_params.yaml", zNear, "zNear");
+    saveYAML("cam_params.yaml", zFar, "zFar");
+}
+
+
+int main(int argc, char* argv[]) {
   // Load all scene-related things
   std::string data_path = CONFIG["data_path"].as<std::string>();
   std::string scene_name = std::string(argv[1]);
@@ -90,6 +190,11 @@ int main(int argc, char* argv[]) {
   // Exposure settings
   float exposure = CONFIG["exposure"].as<float>();
   bool set_exposure = CONFIG["set_exposure"].as<bool>();
+
+  // Get rotation angle if any
+  float rotation_angle = CONFIG["rotation_angle"].as<float>() * DEG2RAD;
+  bool do_rotation = CONFIG["do_rotation"].as<bool>();
+
 
   // Output folder and pose file
   std::string name = scene_name;
@@ -148,38 +253,17 @@ int main(int argc, char* argv[]) {
   }
   saveFloat("exposure.txt", ptexMesh.Exposure());
 
-
-  // Shader for rendering the normal map
-  // const std::string normalShader =
-  //   "#version 330\n"
-  //   "uniform mat4 u_MVP;"
-  //   "uniform mat4 u_MV;"
-  //   "in vec3 v_Position;"
-  //   "in vec3 v_Normal;"
-  //   "out vec3 out_Normal;"
-  //   "void main()"
-  //   "{"
-  //   "  gl_Position = u_MVP * vec4(v_Position, 1.0);"
-  //   "  out_Normal = normalize((u_MV * vec4(v_Normal, 0.0)).xyz);"
-  //   "}";
-
-  // pangolin::GlSlProgram normalShaderProgram = pangolin::GlSlProgram::Compile(
-  //   normalShader, "#version 330\nout vec3 out_Normal;");
-
-
   pangolin::ManagedImage<Eigen::Matrix<uint8_t, 3, 1>> image(width, height);
   pangolin::ManagedImage<float> depthImage(width, height);
   pangolin::ManagedImage<uint16_t> depthImageInt(width, height);
 
   // Setup a camera
   pangolin::OpenGlRenderState s_cam;
-  Eigen::Matrix4d T_camera_world, T_new_old, T_new_old_inv;
+  Eigen::Matrix4d T_camera_world, T_affine, T_backrot, M_intr;
   Eigen::Matrix3d M_cam;
-
-  // Eigen::MatrixXf normalImage(width, height * 3);
+  Eigen::Vector3d incremental_translation, axis1, axis2;
 
   float x_increment, y_increment, z_increment;
-  //float x_max, x_min, y_max, y_min, z_max, z_min; 
   int argmax_idx;
   for (int idx=0; idx < paths.size(); ++idx) {
     argmax_idx = argmax(paths.at(idx)[2], paths.at(idx)[5]);
@@ -188,13 +272,23 @@ int main(int argc, char* argv[]) {
     y_increment = (paths.at(idx)[1+3*argmax_idx] - paths.at(idx)[4-3*argmax_idx]) / float_num; 
     z_increment = (paths.at(idx)[2+3*argmax_idx] - paths.at(idx)[5-3*argmax_idx]) / float_num; 
     
-    // Only translate view
-    T_new_old << 1, 0, 0, x_increment,
-                 0, 1, 0, y_increment,
-                 0, 0, 1, z_increment,
+    // Get incremental translation vector (for each step, apply this translation)
+    incremental_translation = Eigen::Vector3d(x_increment, y_increment, z_increment);
+
+    // Get perpendicular axes to the incremental translation vector
+    generatePerpendicularVectors(incremental_translation, axis1, axis2);
+
+    // Incremental Transformation Matrix
+    T_affine << 1, 0, 0, x_increment,
+                0, 1, 0, y_increment,
+                0, 0, 1, z_increment,
+                0, 0, 0, 1;  
+
+    // Back rotation matrix (initialize as identity)
+    T_backrot << 1, 0, 0, 0,
+                 0, 1, 0, 0,
+                 0, 0, 1, 0,
                  0, 0, 0, 1;
-    
-    T_new_old_inv = T_new_old.inverse();
 
     // Set View matrix
     s_cam = pangolin::OpenGlRenderState(
@@ -212,12 +306,11 @@ int main(int argc, char* argv[]) {
         paths.at(idx)[3*argmax_idx], paths.at(idx)[1+3*argmax_idx], paths.at(idx)[2+3*argmax_idx], 
         pangolin::AxisNegZ));
 
+    // Get Intrinsics for config: (width, height, fx, fy, cx, cy, near, far)
+    storeCamParams(width, height, width / 2.0f, width / 2.0f, (width - 1.0f) / 2.0f, (height - 1.0f) / 2.0f, 0.1f, 100.0f);
+
     // Start at some origin (set by input values)
     T_camera_world = s_cam.GetModelViewMatrix();
-    //std::cout << T_camera_world.col(3) << std::endl;
-    //saveData("projection.csv", M_cam, std::string("INTRINSICS"));
-    //T_camera_world.col(3) = Eigen::Vector4d(paths.at(idx)[0], paths.at(idx)[1], paths.at(idx)[2], 1);
-    //#s_cam.SetModelViewMatrix(T_camera_world);
 
     // render some frames
     for (auto i = 0; i < numFrames; i++) {
@@ -268,26 +361,6 @@ int main(int argc, char* argv[]) {
           pangolin::PixelFormatFromString("RGB24"),
           std::string(filename));
 
-
-      // // Render normals
-      // normalFrameBuffer.Bind();
-      // glPushAttrib(GL_VIEWPORT_BIT);
-      // glViewport(0, 0, width, height);
-      // glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-      // glEnable(GL_CULL_FACE);
-
-      // ptexMesh.Render(s_cam, &normalShaderProgram);
-
-      // glDisable(GL_CULL_FACE);
-
-      // glPopAttrib(); // GL_VIEWPORT_BIT
-      // normalFrameBuffer.Unbind();
-
-      // normalTexture.Download(normalImage.data(), GL_RGB, GL_FLOAT);
-
-      // saveData("normals.csv", normalImage, std::string(filename));
-
       // Render depth
       if (renderDepth) {
         // render depth
@@ -318,9 +391,16 @@ int main(int argc, char* argv[]) {
             std::string(filename), true, 34.0f);
       }
 
-      // Move the camera
-      T_camera_world = T_camera_world * T_new_old_inv;
+      // First transform transformation matrix, then Move the camera to desired position
+      if (do_rotation) {
+        // Rotate the camera
+        randomRotation(rotation_angle, axis1, axis2, incremental_translation, T_backrot, T_affine);
+      }
 
+      // Apply incremental translation (+ rotation if enabled)
+      T_camera_world = T_camera_world * T_affine.inverse();
+
+      // Set View matrix
       s_cam.SetModelViewMatrix(T_camera_world);
     }
     std::cout << "\rRendering frame " << numFrames << "/" << numFrames << "... done" << std::endl;
